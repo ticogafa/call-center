ba955de54883802af511a6fa8b73b3e0de34e14a
xTvini <142419660+xTvini@users.noreply.github.com>
2023-11-08 08:43:48 -0300

    bliblioteca

--- biblioteca-rodrigo.py
+++ biblioteca-rodrigo.py
@@ -1,9 +1,2 @@
 import os
-os.system('cls')
-import webbrowser
-
-# URL do vídeo "Never Gonna Give You Up" no YouTube
-url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-
-# Abre o navegador web padrão para exibir o vídeo
-webbrowser.open(url)
+os.system('cls')        
